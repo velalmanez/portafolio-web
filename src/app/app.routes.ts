c248ea4b7e9d6fbf1bbad40067a2d1055a9721e0
Ivan Velazquez <velalmanez@gmail.com>
2026-05-16 14:27:57 -0600

    update: project config

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,3 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [
-    {path: 'home', loadComponent: () => import('./pages/home/home').then(m => m.HomeComponent)},
-    {path:'', redirectTo: '/home', pathMatch: 'full'},
-];
+export const routes: Routes = [];
